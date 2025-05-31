02dbe4c0812851f4d9f6196e4a23484a409ea98c
Ilya <yagafov08@gmail.com>
2025-05-31 11:19:52 +0400

    add reviews

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+const scrollContainer = document.getElementById("scroll-container");
+const btnLeft = document.getElementById("scroll-left");
+const btnRight = document.getElementById("scroll-right");
+
+// Получает ширину одного блока включая отступ
+function getScrollStep() {
+  const item = scrollContainer.querySelector(".item");
+  const style = getComputedStyle(item);
+  const gap = parseInt(style.marginRight || 0) || 10; // fallback
+  return item.offsetWidth + gap;
+}
+
+btnLeft.addEventListener("click", () => {
+  scrollContainer.scrollLeft -= getScrollStep();
+});
+
+btnRight.addEventListener("click", () => {
+  scrollContainer.scrollLeft += getScrollStep();
+});
