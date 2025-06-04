fbbbe4e9a27c6436911fa82a6691cb4d85413892
Ilya <yagafov08@gmail.com>
2025-06-04 19:01:13 +0400

    full adaptive

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,30 @@
+// Modal
+
+function openModal() {
+    document.getElementById("modal").style.top = "0px";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.top = "-400px";
+}
+
+// Links
+
+const navLink = document.querySelectorAll(".nav-link");
+
+for (const link of navLink) {
+link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+e.preventDefault();
+const href = this.getAttribute("href");
+
+document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+});
+}
+
 // Reviews
 
 const scrollContainer = document.getElementById("scroll-container");
